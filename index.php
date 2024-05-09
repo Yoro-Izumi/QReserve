0c2e9f9c59f7c329edfef872d91537d1fae85c8f
deyben <148093188+yojibeans@users.noreply.github.com>
2024-05-09 23:00:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bevitore Sta Rosa</title>
+
+  <!-- <link rel="stylesheet" href="src/css/landing.css"> -->
+  <link rel="stylesheet" href="src/css/style.css">
+
+  <!-- Fontawesome Link for Icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+  <!-- Bootstrap 5 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+  <!-- Montserrat Font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Akronim&family=Anton&family=Aoboshi+One&family=Audiowide&family=Black+Han+Sans&family=Braah+One&family=Bungee+Outline&family=Hammersmith+One&family=Krona+One&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+
+  <link rel="icon" href="src/images/Bevitore-logo.png" type="image/x-icon">
+</head>
+
+<body class="index-landing">
+  <div class="container-fluid homepage">
+    <div class="home">
+      <img src="src/images/Bevitore Billiards Hall Logo.png" alt="Bevitore Logo" class="bevitore-logo">
+      <h1 class="qreserve" id="index-qreserve">QReserve</h1>
+      <h6 class="bevitore">BEVITORE SANTA ROSA</h6>
+      <a href="login.php" type="button" class="btn btn-primary fw-bold start-button" id="index-button">Click to start session</a>
+    </div>
+  </div>
+</body>
+
+</html>
