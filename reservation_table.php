688248d78965ca212f1917932a897f85e63d3c09
Izumi <162671709+Yoro-Izumi@users.noreply.github.com>
2024-06-12 15:41:28 +0000

    walk-details can now be viewed in the reservation table and the action button is not available if user is an admin and not super admin

--- reservation_table.php
+++ reservation_table.php
@@ -1,14 +1,19 @@
-<?php
+<?php 
 session_start();
+include "connect_database.php";
+include "encodeDecode.php";
+include "src/get_data_from_database/get_pool_table_info.php";
+include "src/get_data_from_database/get_reservation_info.php";
+include "src/get_data_from_database/get_walk_in.php";
+include "src/get_data_from_database/get_customer_information.php";
+include "src/get_data_from_database/get_member_account.php";
+
+$key = "TheGreatestNumberIs73";
 date_default_timezone_set('Asia/Manila');
 
-  include "connect_database.php";
-  include "src/get_data_from_database/get_reservation_info.php";
-  include "src/get_data_from_database/get_member_account.php";
-  include "encodeDecode.php";
-  $key = "TheGreatestNumberIs73";
+if(isset($_SESSION['userSuperAdminID'])){
 
-echo"
+echo "
 <thead>
 <tr>
   <th>Actions</th>
@@ -22,6 +27,7 @@ echo"
 </tr>
 </thead>
 <tbody>";
+
 foreach ($arrayReservationInfo as $reservations) {
   $reservationDate = $reservations['reservationDate'];
   $reservationStatus = $reservations['reservationStatus'];
@@ -29,20 +35,15 @@ foreach ($arrayReservationInfo as $reservations) {
   $reservationTimeEnd = $reservations['reservationTimeEnd'];
   $tableNumber = $reservations['poolTableNumber'];
 
-  // minove ko lang to ayaw kasi lumabas sa table
   foreach ($arrayMemberAccount as $members) {
     if ($members['memberID'] == $reservations['memberID']) {
       $customerName = decryptData($members['customerFirstName'], $key) . " " . decryptData($members['customerMiddleName'], $key) . " " . decryptData($members['customerLastName'], $key);
       $contactNumber = decryptData($members['customerNumber'], $key);
       $email = decryptData($members['customerEmail'], $key);
-    } 
-    // else {
-    //   $customerName = "";
-    //   $contactNumber = "";
-    //   $email = "";
-    // }
+    }
   }
-echo"
+
+  echo "
   <tr>
     <td><input type='checkbox' class='reservation-checkbox' value='{$reservations['reservationID']}'></td>
     <td>$customerName</td>
@@ -58,8 +59,8 @@ echo"
     } else {
       $status = "badge bg-danger";
     }
-echo "<td><span class='$status'>$reservationStatus</span></td>
-";
+  echo "<td><span class='$status'>$reservationStatus</span></td>
+  </tr>";
 }
 
 foreach ($arrayWalkinDetails as $walkin) {
@@ -69,38 +70,112 @@ foreach ($arrayWalkinDetails as $walkin) {
   $walkinTimeEnd = $walkin['walkinTimeEnd'];
   $tableNumber = $walkin['poolTableNumber'];
 
-  // minove ko lang to ayaw kasi lumabas sa table
-  foreach ($arrayCustomerInformation as $customer) {
-    if ($customer['memberID'] == $walkin['customerID']) {
-      $customerName = decryptData($members['customerFirstName'], $key) . " " . decryptData($customer['customerMiddleName'], $key) . " " . decryptData($customer['customerLastName'], $key);
-      $contactNumber = decryptData($customer['customerNumber'], $key);
-      $email = decryptData($members['customerEmail'], $key);
-    } 
-    // else {
-    //   $customerName = "";
-    //   $contactNumber = "";
-    //   $email = "";
-    // }
-  }
-echo"
+  // Directly access customer information from the walk-in array
+  $customerName = decryptData($walkin['customerFirstName'], $key) . " " . decryptData($walkin['customerMiddleName'], $key) . " " . decryptData($walkin['customerLastName'], $key);
+  $contactNumber = decryptData($walkin['customerNumber'], $key);
+  $email = decryptData($walkin['customerEmail'], $key);
+
+  echo "
   <tr>
-    <td><input type='checkbox' class='reservation-checkbox' value='{$reservations['reservationID']}'></td>
+    <td><input type='checkbox' class='reservation-checkbox' value='{$walkin['walkinID']}'></td>
     <td>$customerName</td>
-    <td>$reservationDate</td>
-    <td>$reservationTimeStart - $reservationTimeEnd</td>
+    <td>$walkinDate</td>
+    <td>$walkinTimeStart - $walkinTimeEnd</td>
     <td>$tableNumber</td>
     <td>$contactNumber</td>
     <td>$email</td>";
-    if ($reservationStatus == "Paid" || $reservationStatus == "Done") {
+    if ($walkinStatus == "Paid" || $walkinStatus == "Done") {
       $status = "badge bg-success";
-    } else if ($reservationStatus == "On Process" || $reservationStatus == "Pending") {
+    } else if ($walkinStatus == "Waiting" || $walkinStatus == "Pending") {
       $status = "badge bg-warning";
     } else {
       $status = "badge bg-danger";
     }
-echo "<td><span class='$status'>$reservationStatus</span></td>
-
+  echo "<td><span class='$status'>$walkinStatus</span></td>
   </tr>";
 }
 echo "</tbody>";
+}
+else if(isset($_SESSION['userAdminID'])){
+
+
+  echo "
+  <thead>
+  <tr>
+    <th>Name</th>
+    <th>Date of Reservation</th>
+    <th>Time of Reservation</th>
+    <th>Pool Table</th>
+    <th>Contact Number</th>
+    <th>Email Address</th>
+    <th>Status</th>
+  </tr>
+  </thead>
+  <tbody>";
+
+  foreach ($arrayReservationInfo as $reservations) {
+    $reservationDate = $reservations['reservationDate'];
+    $reservationStatus = $reservations['reservationStatus'];
+    $reservationTimeStart = $reservations['reservationTimeStart'];
+    $reservationTimeEnd = $reservations['reservationTimeEnd'];
+    $tableNumber = $reservations['poolTableNumber'];
+
+    foreach ($arrayMemberAccount as $members) {
+      if ($members['memberID'] == $reservations['memberID']) {
+        $customerName = decryptData($members['customerFirstName'], $key) . " " . decryptData($members['customerMiddleName'], $key) . " " . decryptData($members['customerLastName'], $key);
+        $contactNumber = decryptData($members['customerNumber'], $key);
+        $email = decryptData($members['customerEmail'], $key);
+      }
+    }
+
+    echo "
+    <tr>
+      <td>$customerName</td>
+      <td>$reservationDate</td>
+      <td>$reservationTimeStart - $reservationTimeEnd</td>
+      <td>$tableNumber</td>
+      <td>$contactNumber</td>
+      <td>$email</td>";
+      if ($reservationStatus == "Paid" || $reservationStatus == "Done") {
+        $status = "badge bg-success";
+      } else if ($reservationStatus == "On Process" || $reservationStatus == "Pending") {
+        $status = "badge bg-warning";
+      } else {
+        $status = "badge bg-danger";
+      }
+    echo "<td><span class='$status'>$reservationStatus</span></td>
+    </tr>";
+  }
+
+  foreach ($arrayWalkinDetails as $walkin) {
+    $walkinDate = $walkin['walkinDate'];
+    $walkinStatus = $walkin['walkinStatus'];
+    $walkinTimeStart = $walkin['walkinTimeStart'];
+    $walkinTimeEnd = $walkin['walkinTimeEnd'];
+    $tableNumber = $walkin['poolTableNumber'];
+
+    // Directly access customer information from the walk-in array
+    $customerName = decryptData($walkin['customerFirstName'], $key) . " " . decryptData($walkin['customerMiddleName'], $key) . " " . decryptData($walkin['customerLastName'], $key);
+    $contactNumber = decryptData($walkin['customerNumber'], $key);
+    $email = decryptData($walkin['customerEmail'], $key);
+
+    echo "
+    <tr>
+      <td>$customerName</td>
+      <td>$walkinDate</td>
+      <td>$walkinTimeStart - $walkinTimeEnd</td>
+      <td>$tableNumber</td>
+      <td>$contactNumber</td>
+      <td>$email</td>";
+      if ($walkinStatus == "Paid" || $walkinStatus == "Done") {
+        $status = "badge bg-success";
+      } else if ($walkinStatus == "Waiting" || $walkinStatus == "Pending") {
+        $status = "badge bg-warning";
+      } else {
+        $status = "badge bg-danger";
+      }
+    echo "<td><span class='$status'>$walkinStatus</span></td>
+    </tr>";
+  }
+}
 ?>
