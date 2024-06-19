c22ee3498475e2136121de08193387cb681d8443
ao3xjh <148093188+ao3xjh@users.noreply.github.com>
2024-06-19 14:08:00 +0800

    added validations on add new admin

--- src/js/add_new_admin.js
+++ src/js/add_new_admin.js
@@ -1,158 +1,249 @@
-$(document).ready(function() {
-  // Function to update table content
-  function updateTable() {
-    $.ajax({
-      url: 'pool_table.php',
-      type: 'GET',
-      success: function(response) {
-        $('#updateTable').html(response);
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
       }
-    });
-  }
 
-  // Initial table update
-  updateTable();
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
 
-  // Refresh table every 5 seconds
-  setInterval(updateTable, 1000); // Adjust interval as needed
-});
 
-//   For trimming whitespacecs
+//   For trimming whitespaces
 function handleInput(event) {
-  const inputValue = event.target.value.trim(); // Remove leading and trailing whitespaces
-  const lastChar = inputValue.slice(-1); // Get the last character of the input
+  const inputValue = event.target.value;
+  event.target.value = inputValue.trim(); // Remove leading and trailing whitespaces
+}
 
-  // Check if the input is only whitespaces and it's not the last character
-  if (inputValue === '' || (inputValue === ' ' && lastChar !== ' ')) {
-    event.target.value = ''; // Clear the input if it's only whitespaces
+
+// For first names that it wont accept any numeric and special characters
+function validateName(event) {
+  const regex = /^[A-Za-z\s]*$/; // Allow only alphabetic characters and spaces
+  if (!regex.test(event.target.value)) {
+    event.target.value = event.target.value.replace(/[^A-Za-z\s]/g, '');
   }
 }
 
-//   Updated script for password toggle
-document.addEventListener("DOMContentLoaded", function() {
-  const togglePassword1 = document.querySelector("#password-toggle-1");
-  const passwordInput1 = document.querySelector("#password");
-  const eyeIcon1 = togglePassword1.querySelector("i");
-
-  togglePassword1.addEventListener("click", function() {
-    const type =
-      passwordInput1.getAttribute("type") === "password" ?
-      "text" :
-      "password";
-    passwordInput1.setAttribute("type", type);
-
-    // Toggle eye icon classes
-    eyeIcon1.classList.toggle("fa-eye-slash");
-    eyeIcon1.classList.toggle("fa-eye");
-  });
-
-  const togglePassword2 = document.querySelector("#password-toggle-2");
-  const passwordInput2 = document.querySelector("#confirmPassword");
-  const eyeIcon2 = togglePassword2.querySelector("i");
-
-  togglePassword2.addEventListener("click", function() {
-    const type =
-      passwordInput2.getAttribute("type") === "password" ?
-      "text" :
-      "password";
-    passwordInput2.setAttribute("type", type);
-
-    // Toggle eye icon classes
-    eyeIcon2.classList.toggle("fa-eye-slash");
-    eyeIcon2.classList.toggle("fa-eye");
-  });
-});
-
-//   For password checking
-document.addEventListener("DOMContentLoaded", function() {
-  const passwordInput = document.querySelector("#password");
-  const confirmPasswordInput = document.querySelector("#confirmPassword");
-  const passwordMatchFeedback = document.querySelector("#passwordMatchFeedback");
-  const passwordMatch = document.querySelector("#passwordMatch");
-  const passwordMismatch = document.querySelector("#passwordMismatch");
-
-  confirmPasswordInput.addEventListener("input", function() {
-    const password = passwordInput.value;
-    const confirmPassword = confirmPasswordInput.value;
-
-    if (password === confirmPassword) {
-      passwordMatchFeedback.innerHTML = "";
-      passwordMatch.style.display = "block";
-      passwordMismatch.style.display = "none";
-    } else {
-      passwordMatchFeedback.innerHTML = "";
-      passwordMatch.style.display = "none";
-      passwordMismatch.style.display = "block";
-    }
-  });
-
-  passwordInput.addEventListener("input", function() {
-    const password = passwordInput.value;
-    const confirmPassword = confirmPasswordInput.value;
-
-    if (password === confirmPassword) {
-      passwordMatchFeedback.innerHTML = "";
-      passwordMatch.style.display = "block";
-      passwordMismatch.style.display = "none";
-    } else {
-      passwordMatchFeedback.innerHTML = "";
-      passwordMatch.style.display = "none";
-      passwordMismatch.style.display = "block";
-    }
-  });
-
-});
+// For username
+function validateUsername(event) {
+  const regex = /^[a-zA-Z0-9._%+-]*$/; // Allow only alphabetic characters and spaces
+  if (!regex.test(event.target.value)) {
+    event.target.value = event.target.value.replace(/[^a-zA-Z0-9._%+-]/g, '');
+  }
+}
 
 
+//For Contact Number
+function validateContactNumber(event) {
+  const input = event.target;
+  const value = input.value;
 
- //add admin
-//    $(document).ready(function() {
-//     $('#submitAdmin').click(function(e) {
-//       e.preventDefault();
-//       $('#confirm-add-new-admin-modal').modal('show');
-//     });
+  // Allow only numeric characters
+  input.value = value.replace(/[^0-9]/g, '');
+
+  // Check if the length is exactly 11 and starts with '09'
+  if (input.value.length === 11 && input.value.startsWith('09')) {
+    input.setCustomValidity(''); // Valid input
+  } else {
+    input.setCustomValidity('Please provide a valid contact number (11 digits, starts with 09).');
+  }
+}
+
+//For Email 
+function validateEmail(event) {
+  var emailInput = event.target.value;
+
+  event.target.value = emailInput.replace(/\s+/g, '');
+
+  emailInput = event.target.value.trim();
+
+  var isValid = /^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(emailInput);
+
+  if (!isValid) {
+    document.getElementById("emailError").style.display = "block";
+    event.target.setCustomValidity("Please enter a valid email address.");
+  } else {
+    document.getElementById("emailError").style.display = "none";
+    event.target.setCustomValidity("");
+  }
+}
+
+
+document.getElementById("email").addEventListener("input", validateEmail);
+
+
+  //   Updated script for password toggle
+  document.addEventListener("DOMContentLoaded", function() {
+    const togglePassword1 = document.querySelector("#password-toggle-1");
+    const passwordInput1 = document.querySelector("#password");
+    const eyeIcon1 = togglePassword1.querySelector("i");
   
-//     $('#confirm-add-new-admin-modal-button').click(function(e) {
-//       e.preventDefault();
-//       var formData = new FormData($('#add-new-profile-form')[0]);
+    togglePassword1.addEventListener("click", function() {
+      const type =
+        passwordInput1.getAttribute("type") === "password" ?
+        "text" :
+        "password";
+      passwordInput1.setAttribute("type", type);
+  
+      // Toggle eye icon classes
+      eyeIcon1.classList.toggle("fa-eye-slash");
+      eyeIcon1.classList.toggle("fa-eye");
+    });
+  
+    const togglePassword2 = document.querySelector("#password-toggle-2");
+    const passwordInput2 = document.querySelector("#confirmPassword");
+    const eyeIcon2 = togglePassword2.querySelector("i");
+  
+    togglePassword2.addEventListener("click", function() {
+      const type =
+        passwordInput2.getAttribute("type") === "password" ?
+        "text" :
+        "password";
+      passwordInput2.setAttribute("type", type);
+  
+      // Toggle eye icon classes
+      eyeIcon2.classList.toggle("fa-eye-slash");
+      eyeIcon2.classList.toggle("fa-eye");
+    });
+  });
+  
+  //   For password checking
+  document.addEventListener("DOMContentLoaded", function() {
+    const passwordInput = document.querySelector("#password");
+    const confirmPasswordInput = document.querySelector("#confirmPassword");
+    const passwordMatchFeedback = document.querySelector("#passwordMatchFeedback");
+    const passwordMatch = document.querySelector("#passwordMatch");
+    const passwordMismatch = document.querySelector("#passwordMismatch");
+  
+    confirmPasswordInput.addEventListener("input", function() {
+      const password = passwordInput.value;
+      const confirmPassword = confirmPasswordInput.value;
+  
+      if (password === confirmPassword) {
+        passwordMatchFeedback.innerHTML = "";
+        passwordMatch.style.display = "block";
+        passwordMismatch.style.display = "none";
+      } else {
+        passwordMatchFeedback.innerHTML = "";
+        passwordMatch.style.display = "none";
+        passwordMismatch.style.display = "block";
+      }
+    });
+  
+    passwordInput.addEventListener("input", function() {
+      const password = passwordInput.value;
+      const confirmPassword = confirmPasswordInput.value;
+  
+      if (password === confirmPassword) {
+        passwordMatchFeedback.innerHTML = "";
+        passwordMatch.style.display = "block";
+        passwordMismatch.style.display = "none";
+      } else {
+        passwordMatchFeedback.innerHTML = "";
+        passwordMatch.style.display = "none";
+        passwordMismatch.style.display = "block";
+      }
+    });
+  
+  });
 
-//       $.ajax({
-//         type: 'POST',
-//         url: 'admin_crud.php', // Replace 'process_form.php' with the URL of your PHP script
-//         data: formData,
-//         processData: false,
-//         contentType: false,
-//         success: function(response) {
-//           // Handle success response here
-//           //alert(response); // For demonstration purposes, you can display an alert with the response
-//           //location.reload();
-//           $('#success-add-admin-modal').modal('show');
-//         },
-//         error: function(xhr, status, error) {
-//           // Handle error
-//           console.error(xhr.responseText);
-//         }
-//       });
-//     });
-//   });
+    //   SPassword Strength Indicator
+    function checkPasswordStrength(password) {
+      var strength = document.getElementById('password-strength-indicator');
+      var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})");
+      var mediumRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})");
+    
+      if (strongRegex.test(password)) {
+        strength.innerHTML = '<span style="color:green">Strong password</span>';
+      } else if (mediumRegex.test(password)) {
+        strength.innerHTML = '<span style="color:orange">Moderate password</span>';
+      } else {
+        strength.innerHTML = '<span style="color:red">Weak password</span>';
+      }
+    }
 
+
+// For calling the modal
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('booking-form');
+  const submitButton = document.getElementById('submitWalkin');
+  const confirmAddWalkin = new bootstrap.Modal(document.getElementById('confirmAddWalkin'));
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
+    if (form.checkValidity()) {
+      // Form is valid, show success modal
+      confirmAddWalkin.show();
+      // You can also submit the form via AJAX here if needed
+    } else {
+      form.classList.add('was-validated'); // Show validation messages
+    }
+  });
+
+  // Optional: Reset form validation on modal close
+  confirmAddWalkin.addEventListener('hidden.bs.modal', function () {
+    form.classList.remove('was-validated');
+  });
+});
+
+
+//For Modal Body
+$(document).ready(function() {
+  $("#create-walkin-button").click(function() {
+      $("#confirmAddWalkin .modal-body").html(getUserInputs());
+  });
+});
+
+function getUserInputs() {
+  const firstName = $("#firstName").val();
+  const middleName = $("#middleName").val();
+  const lastName = $("#lastName").val();
+  const username = $("#username").val();
+  const contactNumber = $("#contactNumber").val();
+  const email = $("#email").val();
+  const adminSex = $("#adminSex").val();
+  const adminShift = $("#adminShift").val();
+
+
+  return `
+    <div class="modal-content-wrapper">
+        <p><span class="modal-label">Name:</span> <span class="modal-input">${firstName} ${middleName} ${lastName}</span></p>
+        <p><span class="modal-label">Username:</span> <span class="modal-input">${username}</span></p>
+        <p><span class="modal-label">Contact Number:</span> <span class="modal-input">${contactNumber}</span></p>
+        <p><span class="modal-label">Email Address:</span> <span class="modal-input">${email}</span></p>
+        <p><span class="modal-label">Sex:</span> <span class="modal-input">${adminSex}</span></p>
+        <p><span class="modal-label">Shift:</span> <span class="modal-input">${adminShift}</span></p>
+    </div>
+  `;
+}
+
+
+    //add reservation
 $(document).ready(function () {
-  $("#submitAddAdmin").click(function (e) {
+  $("#submitWalkin").click(function (e) {
     e.preventDefault();
 
-    var formData = new FormData($("#add-new-profile-form")[0]);
+    var formData = new FormData($("#booking-form")[0]);
 
     $.ajax({
       type: "POST",
-      url: "admin_crud.php",
+      url: "admin_crud.php", // Replace 'process_form.php' with the URL of your PHP script
       data: formData,
       processData: false,
       contentType: false,
       success: function (response) {
         // Handle success response here
-        // For demonstration purposes, you can display an alert with the response
-        // alert(response); 
-        window.location.href = "add_new_admin.php";
+        //alert(response); // For demonstration purposes, you can display an alert with the response
+        window.location.href = "admin-profiles.php";
       },
       error: function (xhr, status, error) {
         // Handle error
@@ -164,65 +255,6 @@ $(document).ready(function () {
 
 
 
-//reload page
-//   function reloadPage() {
-//     location.reload();
-//   }
-
-
-
-
-
-//   SPassword Strength Indicator
-function checkPasswordStrength(password) {
-  var strength = document.getElementById('password-strength-indicator');
-  var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})");
-  var mediumRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})");
-
-  if (strongRegex.test(password)) {
-    strength.innerHTML = '<span style="color:green">Strong password</span>';
-  } else if (mediumRegex.test(password)) {
-    strength.innerHTML = '<span style="color:orange">Moderate password</span>';
-  } else {
-    strength.innerHTML = '<span style="color:red">Weak password</span>';
-  }
-}
-
-
-
-
-
-
-// Disables the create button
-$(document).ready(function() {
-  // Function to check if all required fields are filled
-  function checkInputs() {
-      var firstName = $('#firstName').val();
-      var lastName = $('#lastName').val();
-      var username = $('#username').val();
-      var adminSex = $('#adminSex').val();
-      var email = $('#email').val();
-      var contactNumber = $('#contactNumber').val();
-      var adminShift = $('#adminShift').val();
-      var password = $('#password').val();
-      var confirmPassword = $('#confirmPassword').val();
-
-      // Enable/disable create button based on input values
-      if (firstName && lastName && username && adminSex && email && contactNumber && adminShift && password && confirmPassword) {
-          $('#create-new-admin-button').prop('disabled', false);
-      } else {
-          $('#create-new-admin-button').prop('disabled', true);
-      }
-  }
-
-  // Call checkInputs on input change
-  $('#firstName, #lastName, #username, #adminSex, #email, #contactNumber, #adminShift, #password, #confirmPassword').on('change', function() {
-      checkInputs();
-  });
-
-  // Initial check on page load
-  checkInputs();
-});
 
 
 
@@ -232,36 +264,21 @@ $(document).ready(function() {
 
 
 
-$(document).ready(function() {
-  $("#create-new-admin-button").click(function() {
-      $("#confirm-add-new-admin-modal .modal-body").html(getUserInputs());
-  });
-});
-
-function getUserInputs() {
-  const firstName = $("#firstName").val();
-  const middleName = $("#middleName").val();
-  const lastName = $("#lastName").val();
-  const username = $("#username").val();
-  const adminSex = $("#adminSex").val();
-  const email = $("#email").val();
-  const contactNumber = $("#contactNumber").val();
-  const adminShift = $("#adminShift").val();
-
-
-  return `
-      <div class="modal-content-wrapper">
-          <p><span class="modal-label">Name:</span> <span class="modal-input">${firstName} ${middleName} ${lastName}</span></p>
-          <p><span class="modal-label">Username:</span> <span class="modal-input">${username}</span></p>
-          <p><span class="modal-label">Sex:</span> <span class="modal-input">${adminSex}</span></p>
-          <p><span class="modal-label">Email Address:</span> <span class="modal-input">${email}</span></p>
-          <p><span class="modal-label">Contact Number:</span> <span class="modal-input">${contactNumber}</span></p>
-          <p><span class="modal-label">Shift:</span> <span class="modal-input">${adminShift}</span></p>
-      </div>
-  `;
-}
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+  
