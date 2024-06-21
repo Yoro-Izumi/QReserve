3d60f76aab8e46b7b12e1fc28153399abc2b14e5
ao3xjh <148093188+ao3xjh@users.noreply.github.com>
2024-06-22 00:53:07 +0800

    fixed

--- edit_admin_account.php
+++ edit_admin_account.php
@@ -21,7 +21,6 @@ if (isset($_SESSION["userSuperAdminID"])) {
     // Retrieve the value of 'passID'
 
     if ($adminAccount['adminInfoID'] == $ID) {
-
       $adminID = $adminAccount['adminID'];
       $adminInfoID = $adminAccount['adminInfoID'];
       $adminUsername = decryptData($adminAccount['adminUsername'], $key);
