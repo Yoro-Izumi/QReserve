7eae567f3ce564717ab33ec8797ed215eec2ae39
ao3xjh <148093188+ao3xjh@users.noreply.github.com>
2024-06-23 23:06:36 +0800

    fix

--- src/get_data_from_database/get_pool_tables.php
+++ src/get_data_from_database/get_pool_tables.php
@@ -65,7 +65,7 @@ foreach ($rearrangedTables as $ptable) {
             <div class="table-number">
                 <?php echo $poolTableNumber; ?>
             </div>
-            <div class="">
+            <div class="table-time">
                 <p><?php echo $status; ?></p>
             </div>
         </div>
