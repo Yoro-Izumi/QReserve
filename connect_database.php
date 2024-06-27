3870fc4217549d4e02d327c2496c8ab064297f72
Izumi <162671709+Yoro-Izumi@users.noreply.github.com>
2024-06-27 17:43:13 +0000

    fixes admin walkin

--- connect_database.php
+++ connect_database.php
@@ -1,7 +1,7 @@
 <?php //connect to database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "qreserve_data";
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12713153";
+$password = "LcbCQaWeEn";
+$dbname = "sql12713153";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 ?>
