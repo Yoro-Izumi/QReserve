82f5b594037c08547882c81f4d9dca5b8cdddfef
Izumi <162671709+Yoro-Izumi@users.noreply.github.com>
2024-06-29 01:52:59 +0000

    alter connection for testing purpose

--- zalter_connect.php
+++ zalter_connect.php
@@ -1,7 +1,7 @@
 <?php //connect to database
 $servername = "sql12.freesqldatabase.com";
-$username = "sql12713153";
-$password = "LcbCQaWeEn";
-$dbname = "sql12713153";
+$username = "sql12716687";
+$password = "jBJuDWbPqK";
+$dbname = "sql12716687";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 ?>
